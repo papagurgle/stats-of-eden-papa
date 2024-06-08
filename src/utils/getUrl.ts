b0c82f4b403ae4b148f002ce63c9be3f846ec9ed
mmdevcodes <20646367+mmdevcodes@users.noreply.github.com>
2024-06-07 21:47:13 -0500

    Merge branch 'dev'

--- src/utils/getUrl.ts
+++ src/utils/getUrl.ts
@@ -1,14 +1,17 @@
 const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
     : 'http://localhost:3000';
 
 const INCLUDES_FORWARD_SLASH_AT_START_REGEX = /^\/(.|\n)*$/;
 const INCLUDES_FORWARD_SLASH_AT_START = (string: string) =>
   INCLUDES_FORWARD_SLASH_AT_START_REGEX.test(string);
 
-const getUrl = (path: string) =>
-  `${BASE_URL}${!INCLUDES_FORWARD_SLASH_AT_START(path) ? '/' : ''}${path}`;
+const getUrl = (path: string) => {
+  const url = `${BASE_URL}${!INCLUDES_FORWARD_SLASH_AT_START(path) ? '/' : ''}${path}`;
+
+  return url;
+};
 
 export default getUrl;
 export { BASE_URL, INCLUDES_FORWARD_SLASH_AT_START, INCLUDES_FORWARD_SLASH_AT_START_REGEX };
